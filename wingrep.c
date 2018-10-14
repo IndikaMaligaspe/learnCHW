a26664d8a206b435212d554205cd113aa4d09b80
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2018-10-14 18:53:56 +0530

    Commit changes as at today

--- wingrep.c
+++ wingrep.c
@@ -34,16 +34,40 @@ int read_and_find_string(char *file_name, char *find_what)
 	debug("Size of file : %d",size);
 	//Go to the begning of file
 	rewind(file);
-	if (size < BUFF_SIZE)
+	if (size > BUFF_SIZE)
 		read_chunk = BUFF_SIZE;
 
 	char *buf = malloc(read_chunk);
 	size_t nread;
+	int i = 0;
+	char *read_line = malloc(sizeof(buf));
+	strcpy(read_line,"");
+	while((nread = fread(buf,sizeof(buf),1,file)) > 0){
+		i = i+1;
+		debug("iteration - %d size of read_line - %d\n",i,sizeof(read_line) );
+		if(buf[sizeof(buf)-1] == '\n'){
+			debug("starting new line.... with iteration %d",i);
+			int line_size = sizeof(read_line)+sizeof(buf)+1;
+			debug("line size - %d ",line_size);
+			char *line = malloc(line_size);
+			debug("line allocated");
+			strcpy(line,read_line);
+			strcat(line,buf);
+			printf(line);
+			free(line);
+			free(read_line);
+			read_line = malloc(read_chunk);
+		}else{
+			debug("starting chunk...");
+			read_line = realloc(read_line,(sizeof(buf)*i));
+			debug("realoocation done... with size - %d",sizeof(read_line));
+			strcat(read_line,buf);
+		}	
 
-	while((nread = fread(buf,1,sizeof(buf),file)) > 0){
-		printf("%s",buf);
 	}
+
 	free(buf);
+	free(read_line);
 	fclose(file);
 
 
