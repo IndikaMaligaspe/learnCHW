9bf67d53ee085e3d29ad695f880f5d6d01bd72b5
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2018-10-16 06:39:53 +0530

    with a new makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,63 @@
 CFLAGS=-Wall -g
 
+ifeq ($(shell echo "check_quotes"),"check_quotes")
+WINDOWS := yes
+clean:
+	del exer1.exe
+	del ex3.exe
+	del ex8.exe
+	del ex9.exe
+	del ex10.exe
+	del ex11.exe
+	del ex12.exe
+	del ex13.exe
+	del ex14.exe
+	del ex15.exe
+	del ex16.exe
+	del ex17.exe
+	del ex18.exe
+	del ex19.exe
+	del ex22.o
+	del ex22_main.exe
+	del ex23.exe
+all:
+	del exer1.exe
+	del ex3.exe
+	del ex8.exe
+	del ex9.exe
+	del ex10.exe
+	del ex11.exe
+	del ex12.exe
+	del ex13.exe
+	del ex14.exe
+	del ex15.exe
+	del ex16.exe
+	del ex17.exe
+	del ex18.exe
+	del ex19.exe
+	del ex22.o
+	del ex22_main.exe
+	del ex23.exe
+	del wingrep.exe
+	gcc exer1.c -o exer1
+	gcc ex3.c -o ex3
+	gcc ex8.c -o ex8
+	gcc ex9.c -o ex9
+	gcc ex10.c -o ex10
+	gcc ex11.c -o ex11
+	gcc ex12.c -o ex12
+	gcc ex13.c -o ex13
+	gcc ex14.c -o ex14
+	gcc ex15.c -o ex15
+	gcc ex16.c -o ex16
+	gcc ex17.c -o ex17
+	gcc ex18.c -o ex18
+	gcc ex19.c -o ex19
+	gcc -Wall -g -DNDEBUG -c -o ex22.o ex22.c
+	gcc -Wall -g -DNDEBUG ex22_main.c ex22.o -o ex22_main
+	gcc ex23.c -o ex23
+	gcc wingrep.c -o wingrep	
+else
 clean:
 	rm -f exer1
 	rm -f ex3
@@ -18,7 +76,6 @@ clean:
 	rm -f ex22.o
 	rm -f ex22_main
 	rm -f ex23
-	
 all:
 	rm -f exec1
 	rm -f ex3
@@ -56,3 +113,10 @@ all:
 	gcc -Wall -g -DNDEBUG ex22_main.c ex22.o -o ex22_main
 	gcc ex23.c -o ex23
 	gcc wingrep.c -o wingrep
+WINDOWS := no
+
+endif
+
+
+	
+
--- wingrep.c
+++ wingrep.c
@@ -20,10 +20,9 @@ int read_and_find_string(char *file_name, char *find_what)
 	//Get the size of the file
 	line = malloc(BUFF_SIZE);
 	while((len = getline(&line ,file)) != -1){
-		printf("%s",line);
-		fseek(file,len,0);
+		printf("%s \n",line);
+    strcpy(line,"\n");
 	}
-
 	fclose(file);
 	if(line)
 		free(line);
@@ -36,7 +35,7 @@ error:
 
 int getline(char **line , FILE *stream)
 {
-  char *_line = NULL;
+  char line_buffer[BUFF_SIZE] = {""};
   char c;
   if (stream==NULL)
   	return -1;
@@ -44,34 +43,34 @@ int getline(char **line , FILE *stream)
   if (c=='\0')
   	return -1;
   if (c == '\n')
-  	return -1;
+  	return 1;
   if (c == EOF)
   	return -1;
 
   int buf_size = 0;	 
   char buffer[1];	
-  _line = malloc(BUFF_SIZE);
+
+  int i =0;
   while (c != EOF){
-  	buffer[0] = (char) c;
-    strcpy(_line,buffer);
+  	line_buffer[i] = (char) c;
+    // printf("------- %c", (char)c);
     buf_size++;
     if (c == '\n'){
-    	printf("************************\n");
-    	printf("%s", _line);
-    	return -1;
+      line_buffer[i] = '\0';
+      strcpy(*line,line_buffer);
+    	return i;
     }
-    	
-
     c = fgetc(stream);
+    i++;
   }
-  return -1;
+  return 1;
 }
 
 int main(int argc , char *argv[])
 {
-	read_and_find_string(NULL, NULL);
-	read_and_find_string("wingrep.c", NULL);
-	read_and_find_string(NULL,"dbg.h");
+	// read_and_find_string(NULL, NULL);
+	// read_and_find_string("wingrep.c", NULL);
+	// read_and_find_string(NULL,"dbg.h");
 	read_and_find_string("wingrep.c","dbg.h");
 	// read_and_find_string("ex19.c","dbg.h");
 }
